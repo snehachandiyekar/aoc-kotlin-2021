8afd90f5e1c8249658f24bd37adf141ce8feb6ae
snehachandiyekar <sneha22c@gmail.com>
2022-01-19 16:49:36 +0530

    day06

---
+++ app/src/main/java/com/snehachandiyekar/aockotlin2021/Day06.kt
@@ -0,0 +1,39 @@
+package com.snehachandiyekar.aockotlin2021
+
+fun main() {
+    val input = readInputString("Day06.txt")
+    val dayCountList = input[0].split(",").map { it.toInt() }.toMutableList()
+    part1Memo(dayCountList, 0)
+}
+
+
+val noOfDays = 256
+var count = 0L
+var fishMap = emptyMap<Int, Long>().toMutableMap()
+
+fun part1Memo(input: MutableList<Int>, state: Int): Long {
+    for (i in input) {
+        count++
+        if (!fishMap.containsKey(i)) {
+            var tempCount = getChildFishCount(state + i)
+            fishMap[i] = tempCount
+        }
+        count += fishMap[i]!!
+    }
+
+    println("$count, ${fishMap.size}")
+    return count
+}
+
+fun getChildFishCount(state: Int): Long {
+    var runningState = state
+    var tempCount = 0L
+    while (runningState < noOfDays) {
+        tempCount++
+        println("$runningState $tempCount")
+        tempCount += getChildFishCount(runningState + 9)
+        runningState += 7
+    }
+
+    return tempCount
+}
