47e227f4c1fe14fd70c958b74df5dcd5decd9017
snehachandiyekar <sneha22c@gmail.com>
2022-02-22 15:44:19 +0530

    day09 part02

--- app/src/main/java/com/snehachandiyekar/aockotlin2021/Day09.kt
+++ app/src/main/java/com/snehachandiyekar/aockotlin2021/Day09.kt
@@ -1,5 +1,7 @@
 package com.snehachandiyekar.aockotlin2021
 
+import java.util.*
+
 fun main() {
     val input = readInputString("Day09.txt")
 
@@ -32,7 +34,6 @@ fun main() {
 
                 sum += num + 1
                 //println("num: $num ($left,$top,$right,$bottom) sum: $sum ")
-
 
                 /*if (hIndex == 0 && vIndex == 0) {
                     val right = input[hIndex][vIndex + 1].digitToInt()
@@ -143,14 +144,102 @@ fun main() {
         return sum
     }
 
-    fun part2(input: List<Int>): Int {
-        var count = 0
+    fun validateInput(
+        input: Location,
+        locStack: Stack<Location>
+    ) {
+        if (input.num != 9 && !input.isVisited) {
+            input.isVisited = true
+            locStack.push(input)
+        }
+    }
 
-        return count
+    fun part2(input: List<String>): Long {
+        val basinInput = mutableListOf<List<Location>>()
+        val lowPointList = mutableListOf<Location>()
+
+        for (hIndex in input.indices) {
+            val basinInput1 = mutableListOf<Location>()
+
+            for (vIndex in input[hIndex].indices) {
+
+                val num = input[hIndex][vIndex].digitToInt()
+                val left = vIndex - 1
+                val right = vIndex + 1
+                val top = hIndex - 1
+                val bottom = hIndex + 1
+
+                basinInput1.add(Location(num, hIndex, vIndex))
+
+                if (left >= 0 && input[hIndex][left].digitToInt() <= num) {
+                    continue
+                }
+
+                if (right <= input[hIndex].length - 1 && input[hIndex][right].digitToInt() <= num) {
+                    continue
+                }
+
+                if (top >= 0 && input[top][vIndex].digitToInt() <= num) {
+                    continue
+                }
+
+                if (bottom <= input.size - 1 && input[bottom][vIndex].digitToInt() <= num) {
+                    continue
+                }
+
+                lowPointList.add(Location(num, hIndex, vIndex))
+            }
+            basinInput.add(basinInput1)
+        }
+
+        val basinCount = mutableListOf<Int>()
+
+        for (point in lowPointList) {
+            val locStack = Stack<Location>()
+            basinInput[point.x][point.y].isVisited = true
+            locStack.push(basinInput[point.x][point.y])
+
+            var count = 0
+            while (locStack.isNotEmpty()) {
+                val loc = locStack.pop()
+                count++
+                val hIndex = loc.x
+                val vIndex = loc.y
+
+                val left = vIndex - 1
+                val right = vIndex + 1
+                val top = hIndex - 1
+                val bottom = hIndex + 1
+
+                if (left >= 0) {
+                    validateInput(basinInput[hIndex][left], locStack)
+                }
+
+                if (right <= input[hIndex].length - 1) {
+                    validateInput(basinInput[hIndex][right], locStack)
+                }
+
+                if (top >= 0) {
+                    validateInput(basinInput[top][vIndex], locStack)
+                }
+
+                if (bottom <= input.size - 1) {
+                    validateInput(basinInput[bottom][vIndex], locStack)
+                }
+            }
+
+            basinCount.add(count)
+        }
+
+        basinCount.sort()
+        val length = basinCount.size
+        return (basinCount[length - 1] * basinCount[length - 2] * basinCount[length - 3]).toLong()
     }
 
     println(part1(input))
-    //print(part2(input))
+    print(part2(input))
 }
+
+class Location(var num: Int, var x: Int, var y: Int, var isVisited: Boolean = false)
 
 
